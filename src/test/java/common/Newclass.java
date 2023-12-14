8c5a8b1dc200663904998f5649f160d1d1bd4051
Andrews0897 <ryanfoo834@gmail.com>
2023-12-14 17:24:24 +0530

    newcode

--- src/test/java/common/Newclass.java
+++ src/test/java/common/Newclass.java
@@ -4,6 +4,7 @@ public class Newclass {
 	public static void main(String[] args) {
 		System.out.println("git");
 		System.out.println("another push");
+		System.out.println("code by other");
 	}
 
 }
